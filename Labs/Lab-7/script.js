f553aee708d84b0b1f668a5339a1a290eaae4dd9
Ajenaghugrure voweiro <119519861+voweiro@users.noreply.github.com>
2025-10-01 23:33:37 +0300

    assignment3

---
+++ Labs/Lab-7/script.js
@@ -0,0 +1,34 @@
+let firstName = "Voweiro";
+let lastName = "Ajenaghughrure";
+let newsentence = "Hello my name is";
+let num1 = 6;
+let num2 = 3;
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(firstName + num2);
+
+console.log(firstName);
+
+console.log(
+  "Hello I'm " +
+    firstName +
+    ", i am called " +
+    firstName +
+    ", call me   " +
+    firstName +
+    " " +
+    lastName +
+    "  ........  " +
+    firstName
+);
+
+console.log(
+  `Hello I'm ${firstName}, i am called ${firstName}, call me ${firstName} ${lastName}  ........  ${firstName}`
+);
