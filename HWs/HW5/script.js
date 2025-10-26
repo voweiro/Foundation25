578e9c97c8d44fbefc0bcf283339125f17d70894
Ajenaghugrure voweiro <119519861+voweiro@users.noreply.github.com>
2025-10-26 15:20:26 +0200

    assignment HW-5

--- HWs/HW5/script.js
+++ HWs/HW5/script.js
@@ -40,3 +40,275 @@ function calculateSquareRoot(num) {
 
 console.log(calculateSquareRoot(25));
 console.log(calculateSquareRoot(100));
+
+// Email Masking Function
+
+function maskEmail(email) {
+  const atIndex = email.indexOf("@");
+  const username = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
+
+  if (username.length <= 2) {
+    return email;
+  }
+
+  const masked =
+    username[0] +
+    "*".repeat(username.length - 2) +
+    username[username.length - 1];
+
+  return masked + domain;
+}
+
+let email = "apple.pie@example.com";
+
+console.log(maskEmail(email));
+
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+// Loan Qualification Function
+function getLoanMessage(annualIncome, creditScore) {
+  if (
+    creditScore >= minCreditScoreForDuplex &&
+    annualIncome >= minIncomeForDuplex
+  ) {
+    return "You qualify for a duplex, condo, and car loan.";
+  } else if (
+    annualIncome >= minIncomeForCondo &&
+    creditScore >= minCreditScoreForCondo
+  ) {
+    return "You qualify for a condo and car loan.";
+  } else if (
+    annualIncome >= minIncomeForCar &&
+    creditScore >= minCreditScoreForCar
+  ) {
+    return "You qualify for a car loan.";
+  } else {
+    return "You don't qualify for any loans.";
+  }
+}
+const duplexLoanMsg = getLoanMessage(85000, 850);
+const condoLoanMsg = getLoanMessage(65000, 690);
+const carLoanMsg = getLoanMessage(45000, 660);
+const noLoanMsg = getLoanMessage(25000, 550);
+
+console.log(duplexLoanMsg);
+console.log(condoLoanMsg);
+console.log(carLoanMsg);
+console.log(noLoanMsg);
+
+// String Ending Confirmation Function
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target;
+}
+
+console.log(confirmEnding("Bastian", "n"));
+console.log(confirmEnding("Congratulation", "on"));
+console.log(confirmEnding("Connor", "n"));
+console.log(confirmEnding("Open sesame", "same"));
+console.log(confirmEnding("Open sesame", "sage"));
+
+// Lunch Menu Management
+
+let lunches = [];
+
+function addLunchToEnd(array, lunchItem) {
+  array.push(lunchItem);
+  console.log(`${lunchItem} added to the end of the lunch menu.`);
+  return array;
+}
+
+function addLunchToStart(array, lunchItem) {
+  array.unshift(lunchItem);
+  console.log(`${lunchItem} added to the start of the lunch menu.`);
+  return array;
+}
+
+function removeLastLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    let removed = array.pop();
+    console.log(`${removed} removed from the end of the lunch menu.`);
+  }
+  return array;
+}
+
+function removeFirstLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    let removed = array.shift();
+    console.log(`${removed} removed from the start of the lunch menu.`);
+  }
+  return array;
+}
+
+function getRandomLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches available.");
+  } else {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    let randomLunch = array[randomIndex];
+    console.log(`Randomly selected lunch: ${randomLunch}`);
+  }
+}
+
+function showLunchMenu(array) {
+  if (array.length === 0) {
+    console.log("The menu is empty.");
+  } else {
+    console.log(`Menu items: ${array.join(", ")}`);
+  }
+}
+
+//string reversal function
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("Howdy"));
+console.log(reverseString("Greetings from Earth"));
+
+// grocery shopping list
+console.log("Grocery shopping list");
+
+const shoppingList = [];
+
+console.log("It will be nice to have some fruit to eat.");
+
+shoppingList.push("Apples");
+
+function getShoppingListMsg(arr) {
+  return `Current Shopping List: ${arr}`;
+}
+
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Grapes");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It looks like we need to get some cooking oil.");
+
+shoppingList.unshift("Vegetable Oil");
+console.log(getShoppingListMsg(shoppingList));
+
+shoppingList.push("Popcorn", "Beef Jerky", "Potato Chips");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("This looks like too much junk food.");
+
+shoppingList.pop();
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("It might be nice to get a dessert.");
+
+shoppingList.unshift("Chocolate Cake");
+console.log(getShoppingListMsg(shoppingList));
+
+console.log("On second thought, maybe we should be more health conscious.");
+
+shoppingList.shift();
+shoppingList[0] = "Canola Oil";
+console.log(getShoppingListMsg(shoppingList));
+
+// factorial calculator
+let num = 5;
+
+function factorialCalculator(number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+let factorial = factorialCalculator(num);
+let resultMsg = `Factorial of ${num} is ${factorial}`;
+console.log(resultMsg);
+
+//profile lookup
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+//chunky monkey
+function chunkArrayInGroups(arr, size) {
+  let result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+
+//mutation function
+function mutation(arr) {
+  let first = arr[0].toLowerCase();
+  let second = arr[1].toLowerCase();
+  for (let i = 0; i < second.length; i++) {
+    if (first.indexOf(second[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//missing letters function
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.charCodeAt(i + 1) !== str.charCodeAt(i) + 1) {
+      return String.fromCharCode(str.charCodeAt(i) + 1);
+    }
+  }
+  return undefined;
+}
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("abcdefghjklmno"));
+console.log(fearNotLetter("stvwx"));
